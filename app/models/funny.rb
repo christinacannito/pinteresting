94ba321fd6dbb872424ecb0c60de09218adb5edb
Christina Cannito <christinacannito@gmail.com>
2014-07-03 15:03:38 -0400

    working with fixing paperclip error

--- app/models/funny.rb
+++ app/models/funny.rb
@@ -1,10 +1,12 @@
 class Funny < ActiveRecord::Base
 	belongs_to :user
 	
-	has_attached_file :image, 
-		:style => { :medium => "300x300>", :thumb => "100x100>" },
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 		# :storage => :s3, 
-		:bucket => ENV['finallyfunny']
+		# :bucket => 'finallyfunny', 
+		# :s3_credentials => S3_CREDENTIALS
+		# :storage => :s3, 
+		# :bucket => ENV['finallyfunny']
 	# validates :image, presence: true 
 	# validates :description, presence: true	
 	do_not_validate_attachment_file_type :image 
