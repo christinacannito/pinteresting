d52572ebab68513f11f22e9a02d8b52f0f42a99d
Christina Cannito <christinacannito@gmail.com>
2014-07-03 15:17:05 -0400

    added to application.rb file

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,9 @@ module Pinteresting
     # required for heroku
     config.assets.initialize_on_precompile = false
 
- #     config.paperclip_defaults = {
- #            :storage => :s3,
- #            :s3_host_name => 's3-eu-west-1.amazonaws.com'
- # }
+     config.paperclip_defaults = {
+            :storage => :s3,
+            :s3_host_name => 's3-eu-west-1.amazonaws.com'
+ }
   end
 end
