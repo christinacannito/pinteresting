afad7833cfadb8c91ed859ae440ca7dc4ea95c2a
Christina Cannito <christinacannito@gmail.com>
2014-07-03 12:01:54 -0400

    worked with image uploads using paperclip

--- app/models/funny.rb
+++ app/models/funny.rb
@@ -1,7 +1,7 @@
 class Funny < ActiveRecord::Base
 	belongs_to :user
 	
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" } # here add the default url if a person doesn't want to add an image // :default_url => "/images/:style/missing.png"
 	# copied from the paperclip github - will create a thumbnail and full size image
 	# a pin has an attached image
 	do_not_validate_attachment_file_type :image 
